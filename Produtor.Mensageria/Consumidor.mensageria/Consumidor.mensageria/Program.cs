232d2de52deb40f6fced1d16d1f1eb81922cb4c3
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-10-31 20:47:51 -0300

    .

---
+++ Produtor.Mensageria/Consumidor.mensageria/Consumidor.mensageria/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Reflection;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+using var Connection = factory.CreateConnection();
+using var channel = Connection.CreateModel();
+const string fila = "fila_teste";
+
+channel.QueueDeclare(queue: fila,
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, args) =>
+{
+   var body = args.Body.ToArray();
+   var mensagem = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Mensagem recebida {mensagem}...");
+};
+
+channel.BasicConsume(queue: fila,
+    autoAck: true, //esse atributo determina se vai tirar da fila ou n
+    consumer: consumer);
+
+Console.WriteLine("Aguardando mensagem");
+Console.ReadLine();
---
+++ Produtor.Mensageria/Produtor.Mensageria/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { 
+    HostName = "localhost"
+};
+using var Connection = factory.CreateConnection();
+using var channel = Connection.CreateModel();
+const string fila = "fila_teste";
+
+channel.QueueDeclare(queue: fila,
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+int contador = 0;
+
+while (true)
+{
+    string mensagem = $"Mensagem {contador}.";
+    var body = Encoding.UTF8.GetBytes(mensagem);
+
+    channel.BasicPublish(exchange: "",
+        routingKey: fila,
+        basicProperties: null,
+        body: body);
+    Thread.Sleep(3000);
+    contador++;
+}
+
+
+Console.WriteLine("Mensagem postada com sucesso!");
+Console.ReadLine();
